24bd9ddd1c45e9e63c45a23ce84cc67c5188c99b
a122000tw <a122000tw@gmail.com>
2021-12-23 15:01:05 +0800

    s

--- stock.py
+++ stock.py
@@ -6,8 +6,7 @@ api = sj.Shioaji()
 
 # 登入帳號
 api.login(
-    person_id="a122000tw@gmail.com",
-    passwd="love2200023",
+
     contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
 )
 
